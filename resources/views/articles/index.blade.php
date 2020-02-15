84fe3febe8cf8f7287db18cadc540a3d5a2ae744
Fernnado <lamas.fernando1419@gmail.com>
2020-02-15 20:38:57 -0300

    Added index method to ArticlesController also setting up layout

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('content')
+    <ul>
+        @foreach ($articles as $article )
+            <li> {{ $article->title }} </li>
+        @endforeach
+    </ul>
+@endsection
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing Laravel 6 with GraphQL and LightHouse</title>
+    @yield('header')
+</head>
+<body>
+    <div class="content">
+        @yield('content')
+    </div>
+
+    <div class="footer">
+        @yield('footer')
+    </div>
+</body>
+</html>
