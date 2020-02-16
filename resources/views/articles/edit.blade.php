7360dc3d1544cebf00145243d6366e693b28ae8d
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-02-16 19:21:33 -0300

    Fixing cancel behaviour of the update article form

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -28,7 +28,7 @@
                 </div>
 
                 <button type="submit" class="btn btn-primary form-control-sm">Update</button>
-                <a href="{{ url()->previous() }} " class="btn btn-warning form-control-sm">Cancel</a>
+                <a href="{{ route('articles.show', ['article' => $article->id]) }} " class="btn btn-warning form-control-sm">Cancel</a>
 
             </form>
 
