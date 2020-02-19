c78b8a105b600ec4105ec8dd660a7500b6b6c8ab
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-02-19 18:22:19 -0300

    Added some routes to test WSDL using SoapClient

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,28 @@ Route::post('/articles', 'ArticlesController@store')->name('articles.store');
 Route::get('/articles/{article}/edit', 'ArticlesController@edit')->name('articles.edit');
 Route::put('/articles/{article}', 'ArticlesController@update')->name('articles.update');
 Route::delete('/articles/{article}', 'ArticlesController@destroy')->name('articles.destroy');
+
+Route::get('/sancor', function ()
+{
+	// Sancor Webservice
+	$wsdl = 'https://gestionpas.mercantilandina.com.ar/ws_COT05TST/services/cotizarAut/wsdl/cotizarAut.wsdl';
+
+	$cliente = new \SoapClient($wsdl, [
+				// 'login'    => 'usuario',
+				// 'password' => 'contraseña',
+				// 'encoding' => 'UTF-8',
+				'trace'    => true
+			]);
+
+	dd($cliente->__getFunctions());
+});
+
+Route::get('/zurich', function ()
+{
+	$wsdl = 'http://200.5.95.75/preprod/wsintegracioncanales/ServiciosCanales.asmx?WSDL'; // testing (up)
+	// $wsdl = 'https://www.servicioszurich.com.ar/WS/ServiciosCanales.asmx?WSDL'; // production (down)
+
+	$cliente = new \SoapClient($wsdl, ['trace' => true]);
+
+	dd($cliente->__getFunctions());
+});
