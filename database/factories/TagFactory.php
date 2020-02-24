5b798c77bd3f50035707c91d061364a7327fe7d7
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-02-24 19:45:12 -0300

    Using unique tags in factory and migration

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Tag::class, function (Faker $faker)
 {
 	return [
-		'name' => $faker->word
+		'name' => $faker->unique()->word
 	];
 });
--- database/migrations/2020_02_24_202128_create_tags_table.php
+++ database/migrations/2020_02_24_202128_create_tags_table.php
@@ -6,27 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTagsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('tags', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tags', function (Blueprint $table)
+		{
+			$table->bigIncrements('id');
+			$table->string('name')->unique();
+			$table->timestamps();
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('tags');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('tags');
+	}
 }
