9beb957b422fce43482631903956b8b13b85bc8c
Fernando Lamas <lamas.fernando1419@gmail.com>
2020-03-25 12:10:47 -0300

    Added roles and permissions at runtime, before I used tinker for that

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+	/**
+	 * Create a new controller instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
 
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
-    public function index()
-    {
-        return view('home');
-    }
+	/**
+	 * Show the application dashboard.
+	 *
+	 * @return \Illuminate\Contracts\Support\Renderable
+	 */
+	public function index()
+	{
+		Permission::truncate();
+		Role::truncate();
+
+		// create roles:
+		$client = Role::create(['name' => 'client']);
+		$reader = Role::create(['name' => 'reader']);
+
+		// create permissions:
+		$read = Permission::create(['name' => 'test.article.show']);
+		$edit = Permission::create(['name' => 'test.article.edit']);
+
+		// asign permissions to roles:
+		$client->givePermissionTo([$read, $edit]);
+		$reader->givePermissionTo($read);
+
+		return view('home');
+	}
 }
